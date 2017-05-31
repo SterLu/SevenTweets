7162366861ce6900264a317fa02c1611a1aaf73b
Nikola Vukovic <sterlu.sd@gmail.com>
2017-05-31 19:11:29 +0200

    Minor fixes

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 [![Build Status](https://travis-ci.org/SterLu/SevenTweets.svg?branch=master)](https://travis-ci.org/SterLu/SevenTweets)
 
 
-Access [here](http://from.sedamcvrkuta.com:5000/tweets/)
+Access [here](http://from.sedamcvrkuta.com:8000/tweets/)
+
+Initially start with `fab full_deploy:db_user={DATABASE_PASSWORD},db_pw={DATABASE_PASSWORD}`
--- seventweets/__main__.py
+++ seventweets/__main__.py
@@ -1,7 +1,7 @@
 if __name__ == "__main__":
     import sys
 
-    if sys.argv[1] == "storage":
+    if len(sys.argv) > 1 and sys.argv[1] == "storage":
         from seventweets import storage
         Storage = storage.Storage()
         Storage.bootstrap()
@@ -9,4 +9,4 @@ if __name__ == "__main__":
             Storage.add_api_key(sys.argv[2])
     else:
         from seventweets import app
-        app.run(host="0.0.0.0")
+        app.app.run(host="0.0.0.0")
