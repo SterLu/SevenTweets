b3b2b3bc9b754859cfea29cfc5ce0e20a97427e4
Nikola Vukovic <sterlu.sd@gmail.com>
2017-06-01 00:24:06 +0200

    Search + Node storage & registration

--- seventweets/app.py
+++ seventweets/app.py
@@ -7,6 +7,7 @@ from seventweets import storage
 from seventweets.exceptions import error_handled, NotFound, BadRequest
 
 Storage = storage.Storage()
+Nodes = storage.Nodes()
 
 app = Flask(__name__)
 
@@ -52,6 +53,61 @@ def delete_tweet():
             raise NotFound("Tweet not found")
     else:
         raise BadRequest("Missing tweet content")
+
+
+@app.route("/search/<query>", methods=["GET"])
+@error_handled
+def search(query):
+    return json.dumps(Storage.search(query))
+
+
+@app.route("/join_network/", methods=["POST"])
+@error_handled
+def join_network():
+    if not request.data:
+        raise BadRequest("Data not sent")
+    request_data = json.loads(request.data)
+    if request_data['name'] and request_data['address']:
+        print("Initializing current node as '" + request_data['name'] + "' at " + request_data['address'])
+        Nodes.set_self(request_data['name'], request_data['address'])
+        return "Joined"
+    else:
+        raise BadRequest("Missing parameters")
+
+
+@app.route("/network_status/", methods=["GET"])
+@error_handled
+def network_status():
+    return json.dumps(Nodes.get_all())
+
+
+@app.route("/register/", methods=["POST"])
+@error_handled
+def register_node():
+    if not request.data:
+        raise BadRequest("Data not sent")
+    request_data = json.loads(request.data)
+    if request_data['name'] and request_data['address']:
+        self_data = Nodes.get_self()
+        if self_data:
+            print("New node '" + request_data['name'] + "' at " + request_data['address'])
+            Nodes.register_node(request_data['name'], request_data['address'])
+            return json.dumps(Nodes.get_all_external())
+        else:
+            raise NotFound("Network not joined")
+    else:
+        raise BadRequest("Missing parameters")
+
+
+@app.route("/register/<node_name>", methods=["DELETE"])
+@error_handled
+def delete_node(node_name):
+    self_data = Nodes.get_self()
+    if self_data:
+        Nodes.delete_node(node_name)
+        return "", 204
+    else:
+        raise NotFound("Network not joined")
 
 
 if __name__ == "__main__":
--- seventweets/storage.py
+++ seventweets/storage.py
@@ -62,3 +62,57 @@ class Storage:
     @uses_db
     def delete_tweet(self, cursor, tweet_id):
         cursor.execute("DELETE FROM tweets WHERE id=%s", (tweet_id,))
+
+    @uses_db
+    def search(self, cursor, query):
+        cursor.execute("SELECT * FROM tweets WHERE text LIKE '%%{query}%%'".format(query=query))
+        # TODO: Possible SQL injection vulnerability
+        return [row_to_tweet(row) for row in cursor.fetchall()]
+
+
+def row_to_node(row, show_type=False):
+    if not row:
+        return False
+    returns = {
+        "name": row[0],
+        "address": row[1]
+    }
+    if show_type:
+        returns['type'] = row[2]
+    return returns
+
+
+class Nodes:
+    @uses_db
+    def __init__(self, cursor):
+        cursor.execute("DROP TABLE IF EXISTS nodes")
+        cursor.execute("CREATE TABLE IF NOT EXISTS nodes (name TEXT, address TEXT, node_type TEXT)")
+
+    @uses_db
+    def set_self(self, cursor, name, address):
+        cursor.execute("DELETE FROM nodes WHERE node_type ='self'")
+        cursor.execute("INSERT INTO nodes (name, address, node_type) VALUES (%s, %s, 'self')", (name, address))
+
+    @uses_db
+    def get_self(self, cursor):
+        cursor.execute("SELECT * FROM nodes WHERE node_type='self'")
+        return row_to_node(cursor.fetchone())
+
+    @uses_db
+    def get_all(self, cursor):
+        cursor.execute("SELECT * FROM nodes")
+        return [row_to_node(row, True) for row in cursor.fetchall()]
+
+    @uses_db
+    def register_node(self, cursor, name, address):
+        # TODO: Check if node with given name or address exists, update if exists, else insert
+        cursor.execute("INSERT INTO nodes (name, address, node_type) VALUES (%s, %s, 'external')", (name, address))
+
+    @uses_db
+    def get_all_external(self, cursor):
+        cursor.execute("SELECT * FROM nodes WHERE node_type='external'")
+        return [row_to_node(row) for row in cursor.fetchall()]
+
+    @uses_db
+    def delete_node(self, cursor, name):
+        cursor.execute("DELETE FROM nodes WHERE name=%s AND node_type='external'", (name, ))
