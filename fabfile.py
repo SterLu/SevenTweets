eee978e6d0e19415f6a61ac71ced914b5e500a1c
Nikola Vukovic <sterlu.sd@gmail.com>
2017-05-31 17:53:41 +0200

    Reorder fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,12 @@
 from fabric.api import local, settings
 
 
+def full_deploy(db_user, db_pw):
+    initial_setup(db_user, db_pw)
+    deploy(db_user, db_pw)
+    bootstrap_storage()
+
+
 def initial_setup(db_user, db_pw):
     with settings(warn_only=True):
         local('docker network create radionica')
@@ -8,8 +14,26 @@ def initial_setup(db_user, db_pw):
         start_storage(db_user, db_pw)
 
 
-def build():
+def start_storage(db_user, db_pw):
+    with settings(warn_only=True):
+        local('docker stop storage-container')
+    local('docker run --rm -d --name storage-container --net radionica -p 127.0.0.1:5432:5432 \
+           -e POSTGRES_USER={db_user} -e POSTGRES_PASSWORD={db_pw} \
+           -v radionica-postgres-data:/var/lib/postgresql/data postgres:9.6.2'
+          .format(db_user=db_user, db_pw=db_pw))
+
+
+def deploy(db_user, db_pw):
+    build()
+    start(db_user, db_pw)
+
+
+def update():
     local('git pull origin master')
+
+
+def build():
+    update()
     with settings(warn_only=True):
         local('docker stop seventweets-container')
         local('docker rmi seventweets-image')
@@ -22,14 +46,8 @@ def start(db_user, db_pw):
           .format(db_user=db_user, db_pw=db_pw))
 
 
-def deploy(db_user, db_pw):
-    build()
-    start(db_user, db_pw)
-
-
-def full_deploy(db_user, db_pw):
-    initial_setup(db_user, db_pw)
-    deploy(db_user, db_pw)
+def bootstrap_storage():
+    local('docker exec -it seventweets-container python seventweets/storage.py')
 
 
 def stop():
@@ -37,19 +55,5 @@ def stop():
 
 
 def clear():
-    local('docker stop seventweets-container')
     local('docker rmi seventweets-image')
-
-
-def start_storage(db_user, db_pw):
-    with settings(warn_only=True):
-        local('docker stop storage-container')
-    local('docker run --rm -d --name storage-container --net radionica -p 127.0.0.1:5432:5432 \
-           -e POSTGRES_USER={db_user} -e POSTGRES_PASSWORD={db_pw} \
-           -v radionica-postgres-data:/var/lib/postgresql/data postgres:9.6.2'
-          .format(db_user=db_user, db_pw=db_pw))
-    local('python seventweets/storage.py')
-
-
-def update():
-    local('git pull origin master')
+    local('docker stop seventweets-container')
